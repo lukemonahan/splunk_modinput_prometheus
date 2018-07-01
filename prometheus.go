82c55558121fb5ae2934eda0f0a7b3d4ab760ee1
lukemonahan <lukemonahan@gmail.com>
2018-07-01 21:02:12 +1000

    Added code files

---
+++ prometheus.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+  "bytes"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+  "io"
+  "os"
+	"log"
+	"net/http"
+  "math"
+  "regexp"
+  "github.com/prometheus/prometheus/pkg/textparse"
+)
+
+// Structs to hold XML parsing of input from Splunk
+type Input struct {
+	XMLName       xml.Name      `xml:"input"`
+	ServerHost    string        `xml:"server_host"`
+	ServerURI     string        `xml:"server_uri"`
+	SessionKey    string        `xml:"session_key"`
+	CheckpointDir string        `xml:"checkpoint_dir"`
+	Configuration Configuration `xml:"configuration"`
+}
+
+type Configuration struct {
+	XMLName xml.Name `xml:"configuration"`
+	Stanzas []Stanza `xml:"stanza"`
+}
+
+type Stanza struct {
+	XMLName xml.Name `xml:"stanza"`
+	Params  []Param  `xml:"param"`
+	Name    string   `xml:"name,attr"`
+}
+
+type Param struct {
+	XMLName xml.Name `xml:"param"`
+	Name    string   `xml:"name,attr"`
+	Value   string   `xml:",chardata"`
+}
+// End XML structs
+
+// Struct to store final config
+type InputConfig struct {
+  URI         string
+  Match       []string
+	Index       string
+	Sourcetype  string
+	Host        string
+}
+
+func main() {
+
+	if len(os.Args) > 1 {
+		if os.Args[1] == "--scheme" {
+			fmt.Println(DoScheme())
+		} else if os.Args[1] == "--validate-arguments" {
+			ValidateArguments()
+		}
+	} else {
+		Run()
+	}
+
+	return
+}
+
+func DoScheme() string {
+
+	scheme := `<scheme>
+      <title>Prometheus</title>
+      <description>Scrapes a Prometheus endpoint, either directly or via Prometheus federation</description>
+      <use_external_validation>false</use_external_validation>
+      <streaming_mode>simple</streaming_mode>
+      <use_single_instance>false</use_single_instance>
+      <endpoint>
+          <arg name="URI">
+            <title>Metrics URI</title>
+            <description>A Prometheus exporter endpoint</description>
+            <required_on_edit>true</required_on_edit>
+            <required_on_create>true</required_on_create>
+          </arg>
+      </endpoint>
+    </scheme>`
+
+	return scheme
+}
+
+func ValidateArguments() {
+  // Currently unused
+  // Will be used to properly validate in future
+	return
+}
+
+func Config() InputConfig {
+
+	data, _ := ioutil.ReadAll(os.Stdin)
+	var input Input
+	xml.Unmarshal(data, &input)
+
+  var matchExpr = regexp.MustCompile(`^match\.\d+$`)
+
+  var inputConfig InputConfig
+
+	for _, s := range input.Configuration.Stanzas {
+		for _, p := range s.Params {
+			if p.Name == "URI" {
+				inputConfig.URI = p.Value
+			}
+			if p.Name == "index" {
+				inputConfig.Index = p.Value
+			}
+			if p.Name == "sourcetype" {
+				inputConfig.Sourcetype = p.Value
+			}
+			if p.Name == "host" {
+				inputConfig.Host = p.Value
+			}
+      if matchExpr.MatchString(p.Name) {
+        inputConfig.Match = append(inputConfig.Match, p.Value)
+      }
+		}
+	}
+
+  return inputConfig
+}
+
+func Run() {
+
+  // Output of metrics are sent to Splunk via log interface
+  // This ensures parallel requests don't interleave, which can happen using stdout directly
+  output := log.New(os.Stdout, "", 0)
+
+  var inputConfig = Config()
+
+  // A buffer to build out metrics in for this request
+  // We dump it all at once, as we may have index/sourcetype etc. directives and we can't have them separated from the metrics they effect by another request
+  var buffer bytes.Buffer
+
+  buffer.WriteString(fmt.Sprintf("***SPLUNK*** index=%s sourcetype=%s host=%s\n", inputConfig.Index, inputConfig.Sourcetype, inputConfig.Host))
+
+  client := &http.Client{}
+
+  req, err := http.NewRequest("GET", inputConfig.URI, nil)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  q := req.URL.Query()
+  for _, m := range inputConfig.Match {
+    q.Add("match[]", m)
+  }
+  req.URL.RawQuery = q.Encode()
+
+  resp, err := client.Do(req)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // Need to parse metrics out of body individually to convert from scientific to decimal etc. before handing to Splunk
+  p := textparse.New(body)
+
+
+  for  {
+      et, err := p.Next()
+
+      if err != nil {
+        if err == io.EOF {
+          break
+        } else {
+          continue
+        }
+      }
+
+      // Only care about the actual metric series in Splunk for now
+      if et == textparse.EntrySeries {
+        b, ts, val := p.Series()
+
+        if math.IsNaN(val) || math.IsInf(val, 0) {
+					continue
+				} // Splunk won't accept NaN metrics etc.
+				buffer.WriteString(fmt.Sprintf("%s %f %d\n", b, val, ts))
+
+      }
+  }
+
+  output.Print(buffer.String())
+
+  return
+}
---
+++ prometheusrw.go
@@ -0,0 +1,330 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/gobwas/glob"
+	"github.com/gogo/protobuf/proto"
+	"github.com/golang/snappy"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/prompb"
+)
+
+// Structs to hold XML parsing of input from Splunk
+type Input struct {
+	XMLName       xml.Name      `xml:"input"`
+	ServerHost    string        `xml:"server_host"`
+	ServerURI     string        `xml:"server_uri"`
+	SessionKey    string        `xml:"session_key"`
+	CheckpointDir string        `xml:"checkpoint_dir"`
+	Configuration Configuration `xml:"configuration"`
+}
+
+type Configuration struct {
+	XMLName xml.Name `xml:"configuration"`
+	Stanzas []Stanza `xml:"stanza"`
+}
+
+type Stanza struct {
+	XMLName xml.Name `xml:"stanza"`
+	Params  []Param  `xml:"param"`
+	Name    string   `xml:"name,attr"`
+}
+
+type Param struct {
+	XMLName xml.Name `xml:"param"`
+	Name    string   `xml:"name,attr"`
+	Value   string   `xml:",chardata"`
+}
+
+type Feed struct {
+	XMLName xml.Name `xml:"feed"`
+	Keys    []Key    `xml:"entry>content>dict>key"`
+}
+type Key struct {
+	XMLName xml.Name `xml:"key"`
+	Name    string   `xml:"name,attr"`
+	Value   string   `xml:",chardata"`
+}
+
+// End XML structs
+
+// Structs store final config
+type InputConfig struct {
+	BearerToken string
+	Whitelist   []glob.Glob
+	Blacklist   []glob.Glob
+	Index       string
+	Sourcetype  string
+	Host        string
+}
+
+type GlobalConfig struct {
+	ListenAddr string
+	MaxClients int
+	Disabled   bool
+	EnableTLS  bool
+	CertFile   string
+	KeyFile    string
+}
+
+// End config structs
+
+func main() {
+
+	if len(os.Args) > 1 {
+		if os.Args[1] == "--scheme" {
+			fmt.Println(DoScheme())
+		} else if os.Args[1] == "--validate-arguments" {
+			ValidateArguments()
+		}
+	} else {
+		log.Fatal(Run())
+	}
+
+	return
+}
+
+func DoScheme() string {
+
+	scheme := `<scheme>
+      <title>Prometheus Remote Write</title>
+      <description>Listen on a TCP port as a remote write endpoint for the Prometheus metrics server</description>
+      <use_external_validation>false</use_external_validation>
+      <streaming_mode>simple</streaming_mode>
+      <use_single_instance>true</use_single_instance>
+      <endpoint>
+          <arg name="bearerToken">
+            <title>Bearer token</title>
+            <description>A token configured in Prometheus to send via the Authorization header</description>
+            <required_on_edit>true</required_on_edit>
+            <required_on_create>true</required_on_create>
+          </arg>
+          <arg name="whitelist">
+            <title>Whitelist</title>
+            <description>A comma-separated list of glob patterns to match metric names and index (default *)</description>
+            <required_on_edit>false</required_on_edit>
+            <required_on_create>false</required_on_create>
+          </arg>
+          <arg name="blacklist">
+            <title>Blacklist</title>
+            <description>A comma-separated list of glob patterns to match metric names and prevent indexing (default empty). Applied after whitelisting.</description>
+            <required_on_edit>false</required_on_edit>
+            <required_on_create>false</required_on_create>
+          </arg>
+      </endpoint>
+    </scheme>`
+
+	return scheme
+}
+
+func ValidateArguments() {
+  // Currently unused
+  // Will be used to properly validate in future
+	return
+}
+
+func Config() (GlobalConfig, map[string]InputConfig) {
+
+	data, _ := ioutil.ReadAll(os.Stdin)
+	var input Input
+	xml.Unmarshal(data, &input)
+
+	configMap := make(map[string]InputConfig)
+
+	for _, s := range input.Configuration.Stanzas {
+		var inputConfig InputConfig
+		for _, p := range s.Params {
+			if p.Name == "whitelist" {
+				for _, w := range strings.Split(p.Value, ",") {
+					inputConfig.Whitelist = append(inputConfig.Whitelist, glob.MustCompile(w))
+				}
+			}
+			if p.Name == "blacklist" {
+				for _, b := range strings.Split(p.Value, ",") {
+					inputConfig.Blacklist = append(inputConfig.Blacklist, glob.MustCompile(b))
+				}
+			}
+			if p.Name == "bearerToken" {
+				inputConfig.BearerToken = p.Value
+			}
+			if p.Name == "index" {
+				inputConfig.Index = p.Value
+			}
+			if p.Name == "sourcetype" {
+				inputConfig.Sourcetype = p.Value
+			}
+			if p.Name == "host" {
+				inputConfig.Host = p.Value
+			}
+		}
+
+		configMap[inputConfig.BearerToken] = inputConfig
+	}
+	// Default global config
+	var globalConfig GlobalConfig
+	globalConfig.ListenAddr = ":8098"
+	globalConfig.MaxClients = 10
+	globalConfig.Disabled = true
+	globalConfig.EnableTLS = false
+
+	// Get the global configuration
+	tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
+	client := &http.Client{Transport: tr}
+	req, error := http.NewRequest("GET", input.ServerURI+"/services/configs/inputs/prometheusrw", nil)
+	req.Header.Add("Authorization", "Splunk "+input.SessionKey)
+	response, error := client.Do(req)
+	if error != nil {
+		log.Fatal(error)
+	}
+	body, _ := ioutil.ReadAll(response.Body)
+
+	// Parse the global configuration
+	var feed Feed
+	xml.Unmarshal(body, &feed)
+	for _, k := range feed.Keys {
+		if k.Name == "disabled" {
+			globalConfig.Disabled, _ = strconv.ParseBool(k.Value)
+		}
+		if k.Name == "port" {
+			port, _ := strconv.Atoi(k.Value)
+			globalConfig.ListenAddr = fmt.Sprintf(":%d", port)
+		}
+		if k.Name == "maxClients" {
+			maxClients, error := strconv.Atoi(k.Value)
+			if error != nil || maxClients <= 0 {
+				globalConfig.MaxClients = 10
+			} else {
+				globalConfig.MaxClients = maxClients
+			}
+		}
+		if k.Name == "enableTLS" {
+			globalConfig.EnableTLS, _ = strconv.ParseBool(k.Value)
+		}
+		if k.Name == "certFile" {
+			globalConfig.CertFile = strings.Replace(k.Value, "$SPLUNK_HOME", os.Getenv("SPLUNK_HOME"), -1)
+		}
+		if k.Name == "keyFile" {
+			globalConfig.KeyFile = strings.Replace(k.Value, "$SPLUNK_HOME", os.Getenv("SPLUNK_HOME"), -1)
+		}
+	}
+	response.Body.Close()
+
+	return globalConfig, configMap
+}
+
+func Run() error {
+
+	// Output of metrics are sent to Splunk via log interface
+	// This ensures parallel requests don't interleave, which can happen using stdout directly
+	output := log.New(os.Stdout, "", 0)
+
+	// Actual logging (goes to splunkd.log)
+	//infoLog := log.New(os.Stderr, "INFO ", 0)
+	//debugLog := log.New(os.Stderr, "DEBUG ", 0)
+	//errLog := log.New(os.Stderr, "ERROR ", 0)
+
+	globalConfig, configMap := Config()
+
+	if globalConfig.Disabled == true {
+		log.Fatal("Prometheus input globally disabled")
+	}
+
+	// Semaphore to limit to maxClients concurrency
+	sema := make(chan struct{}, globalConfig.MaxClients)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		// Get the bearer token and corresponding config
+		bearerToken := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+
+		if _, ok := configMap[bearerToken]; !ok {
+			http.Error(w, "Bearer token not recognized. Please contact your Splunk admin.", http.StatusUnauthorized)
+			return
+		}
+
+		inputConfig := configMap[bearerToken]
+
+		// This will queue a client if > maxClients are processing
+		sema <- struct{}{}
+		defer func() { <-sema }()
+
+		// A buffer to build out metrics in for this request
+		// We dump it all at once, as we may have index/sourcetype etc. directives and we can't have them separated from the metrics they effect by another request
+		var buffer bytes.Buffer
+
+		buffer.WriteString(fmt.Sprintf("***SPLUNK*** index=%s sourcetype=%s host=%s\n", inputConfig.Index, inputConfig.Sourcetype, inputConfig.Host))
+
+		compressed, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		reqBuf, err := snappy.Decode(nil, compressed)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		var req prompb.WriteRequest
+		if err := proto.Unmarshal(reqBuf, &req); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		for _, ts := range req.Timeseries {
+			m := make(model.Metric, len(ts.Labels))
+
+			for _, l := range ts.Labels {
+				m[model.LabelName(l.Name)] = model.LabelValue(l.Value)
+			}
+
+			whitelisted := false
+			for _, w := range inputConfig.Whitelist {
+				if w.Match(string(m["__name__"])) {
+					whitelisted = true
+				}
+			}
+
+			if !whitelisted {
+				continue
+			}
+
+			blacklisted := false
+			for _, b := range inputConfig.Blacklist {
+				if b.Match(string(m["__name__"])) {
+					blacklisted = true
+				}
+			}
+
+			if blacklisted {
+				continue
+			}
+
+			for _, s := range ts.Samples {
+				if math.IsNaN(s.Value) || math.IsInf(s.Value, 0) {
+					continue
+				} // Splunk won't accept NaN metrics etc.
+				buffer.WriteString(fmt.Sprintf("%s %f %d\n", m, s.Value, s.Timestamp))
+			}
+		}
+
+		output.Print(buffer.String())
+	})
+
+	if globalConfig.EnableTLS == true {
+		return http.ListenAndServeTLS(globalConfig.ListenAddr, globalConfig.CertFile, globalConfig.KeyFile, nil)
+	} else {
+		return http.ListenAndServe(globalConfig.ListenAddr, nil)
+	}
+}
